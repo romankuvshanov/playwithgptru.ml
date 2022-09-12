5230d67746f33c46b77277106022b98c0d4de3d6
romankuvshanov <kuvshanovr@gmail.com>
2022-09-12 20:12:18 +0500

    Second working tech demo. Errors fixed

--- js/main.js
+++ js/main.js
@@ -19,24 +19,72 @@
 //   }
 // })
 
-function generate() {
-  if (document.getElementById("query").value) {
-    //alert((document.getElementById("query").value));
-    document.getElementById("generation").style.display = "block";
-    document.getElementById("generationResultAnimation").style.display = "block";
-    fetch('https://faas-blr1-8177d592.doserverless.co/api/v1/web/fn-0354e1f4-a10b-47ac-93a0-2ffee32414e2/default/playwithgptru?query=' + document.getElementById("query").value)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        document.getElementById("generationResultAnimation").style.display = "none";
-        document.getElementById("generationResultField").innerText = data.predictions;
+async function test() {
+  document.getElementById("generationDiv").style.display = "block";
+  document.getElementById("generationResultAnimation").style.display = "block";
+  await new Promise(r => setTimeout(r, 4000));
+  //document.getElementById("generationResultAnimation").style.animation = "offsettozero 5s linear forwards";
+  document.getElementById("generationResultAnimation").style.strokeDashoffset = "0";
+}
 
-      });
+
+function resetOutputField() {
+  // If launch prediction for the first time
+  if (!document.getElementById("generationDiv").style.display) {
+    document.getElementById("generationDiv").style.display = "block";
+    document.getElementById("generationResultAnimation").style.display = "block";
+  }
+  // Else launch prediction second and more time
+  else {
+    document.getElementById("generationResultField").innerText = "";
+    document.getElementById("generationResultAnimation").style.display = "block";
+  }
+}
+
+function updateResults(result) {
+  if (result === "Ошибка обработки запроса! Попробуйте снова!") {
+    document.getElementById("generationResultField").style.color = "red";
+  }
+  document.getElementById("generationResultAnimation").style.display = "none";
+  document.getElementById("generationResultField").innerText = result;
+  document.getElementById("generate").innerText = "Сгенерировать снова!";
+}
+
+async function generate() {
+  // If we have something in query textarea
+  if (document.getElementById("query").value) {
+    resetOutputField();
+    let result = await getPredictionFromAPI(document.getElementById("query").value);
+    updateResults(result.predictions);
   }
   else {
-    alert("Вы ничего не ввели!");
+    alert("Вы ничего не ввели! Введите ваш запрос в поле под загловком \"Ваше начало\".");
+  }
+}
+
+async function getPredictionFromAPI(query) {
+  const response = await fetch('https://faas-blr1-8177d592.doserverless.co/api/v1/web/fn-0354e1f4-a10b-47ac-93a0-2ffee32414e2/default/playwithgptru?query=' + query);
+  if (response.ok) {
+    return await response.json();
+  }
+  else return { predictions:"Ошибка обработки запроса! Попробуйте снова!" };
+}
+
+
+//https://faas-blr1-8177d592.doserverless.co/api/v1/web/fn-0354e1f4-a10b-47ac-93a0-2ffee32414e2/default/playwithgptru?query=
+function generate2() {
+  if (document.getElementById("query").value) {
+    if (document.getElementById("generationDiv").style.display == 'none') {
+      alert("none");
+    }
+
+    document.getElementById("generationDiv").style.display = "block";
+    document.getElementById("generationResultAnimation").style.display = "block";
+
+     getPredictionFromAPI("query");
+  }
+  else {
+    alert("Вы ничего не ввели! Введите ваш запрос в поле под загловком \"Ваше начало\".");
   }
 
 }
